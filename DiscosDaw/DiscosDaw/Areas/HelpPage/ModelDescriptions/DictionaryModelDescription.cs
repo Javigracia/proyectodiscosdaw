e9ef93a60cecde6fe5f49534f7283db2ef049787
Javigracia <neodreamer@gmail.com>
2017-03-01 22:07:43 +0100

    Borrado de version v.002
    Problemas en el commit de v.003. Borrado de v.002

--- DiscosDaw/DiscosDaw/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DiscosDaw.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- DiscosDaw/DiscosDaw/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DiscosDaw.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
