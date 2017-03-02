ecdf3b1c4e12f3c8e39e62cebed71873e924fc21
Cristo Daniel <danilismo23@gmail.com>
2017-03-02 20:44:58 +0100

    React y Discos
    Integración de los paquetes necesarios para trabajar con React y añadida
    la visualización de los discos por medio de ello

---
+++ DiscosDaw/DiscosDaw/Scripts/jsx/listaDiscos.jsx
@@ -0,0 +1,55 @@
+﻿var ListaDiscos = React.createClass({
+
+    getInitialState: function() {
+        return { result: '' };
+    },
+
+    componentDidMount: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function (data) {
+                var result = { result: data };
+                this.setState(result);
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    render: function () {
+        var disks = [];
+        for (var i = 0; i < this.state.result.length; i++)
+        {
+            var agno = this.state.result[i].Agno || "N/A";
+            disks.push(
+                <tr key={i}>
+                    <td>{this.state.result[i].Titulo}</td>
+                    <td>{this.state.result[i].Interprete}</td>
+                    <td>{agno}</td>
+                    <td>{this.state.result[i].mediaPuntuacion}</td>
+                </tr>
+                );
+    }
+        return (
+        <table className="infoDiscos">
+            <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Intérprete</th>
+                    <th>Año</th>
+                    <th>Valoración</th>
+                </tr>
+            </thead>
+            <tbody>
+                {disks}
+            </tbody>
+        </table>
+        );
+}
+});
+ReactDOM.render(
+  <ListaDiscos url="/api/Vdisco"/>,
+  document.getElementById('content')
+);
