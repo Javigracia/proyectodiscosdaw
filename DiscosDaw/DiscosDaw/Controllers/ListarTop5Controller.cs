96c4c69955bcda5b0b61d83179d197123f894b14
Cristo Daniel <danilismo23@gmail.com>
2017-03-05 00:04:46 +0100

    Añadido Controlador para el top 5

---
+++ DiscosDaw/DiscosDaw/Controllers/ListarTop5Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiscosDaw.Controllers
+{
+    public class ListarTop5Controller : Controller
+    {
+        // GET: ListarTop5
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
